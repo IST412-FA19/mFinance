d8a668883d0ae7e704054cf6034191c615a4bfad
swhealon <swhealon@gmail.com>
2019-09-22 21:45:07 -0500

    Added Loan documentation

---
+++ mFinance/src/edu/psu/ist412/loans/Loan.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.psu.ist412.loans;
+
+/**
+ *
+ * @author swhealon
+ */
+public class Loan {
+    
+    /* The Loan class will contain the information about existing loans and it 
+    will contain the process logic to help issue and maintain loans for customers
+    The API will allow other modules to pull the required information from the loan
+    class.
+    */
+    
+}
