d9e3629f8cb69558992019225ad2ddcb3203f3b9
GRamos <GRamos@192.168.1.13>
2019-10-06 20:08:14 -0400

    Updating customer customerList class

--- mFinance/src/Login/Security.java
+++ mFinance/src/Login/Security.java
@@ -11,11 +11,63 @@ package Login;
  */
 public class Security {
     
-    private User userLog;
+    protected boolean userLog;
+    protected String userKey;
+    protected String userValue;
             
     public Security (User userl){
         
+        if (userl.userName == userKey && userl.userPass==userValue){
+            System.out.print("Login Succesful!");
+            userLog = true;
+        }
+        else {
+            System.out.print("Login unsuccesful, please try again. Confirm your username and password.");
+            userLog = false;
+        }
     
 }
+
+    /**
+     * @return the userLog
+     */
+    public boolean isUserLog() {
+        return userLog;
+    }
+
+    /**
+     * @return the userKey
+     */
+    public String getUserKey() {
+        return userKey;
+    }
+
+    /**
+     * @return the userValue
+     */
+    public String getUserValue() {
+        return userValue;
+    }
+
+    /**
+     * @param userLog the userLog to set
+     */
+    public void setUserLog(boolean userLog) {
+        this.userLog = userLog;
+    }
+
+    /**
+     * @param userKey the userKey to set
+     */
+    public void setUserKey(String userKey) {
+        this.userKey = userKey;
+    }
+
+    /**
+     * @param userValue the userValue to set
+     */
+    public void setUserValue(String userValue) {
+        this.userValue = userValue;
+    }
     
 }
