a266dea6e277ec7aedd7fc8a964a12926a1483d9
GRamos <GRamos@192.168.1.13>
2019-10-06 20:22:02 -0400

    Updating customer customerList class

--- mFinance/src/Login/Authenticator.java
+++ mFinance/src/Login/Authenticator.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package Login;
+import java.util.Random;
 
 /**
  *
  * @author Randi Semera
  */
 public class Authenticator {
+    
+    private String authKey;
+    protected int ranNum;
+    
+    public Authenticator(Security userLogin){
+        
+        Random rand = new Random();
+        
+        ranNum = rand.nextInt(9000000) + 1000000;
+        
+        if (userLogin.userLog == true){
+            this.authKey = Integer.toString(ranNum);
+        }
+        else{
+            this.authKey = null;
+        }
+        
+    }
     
 }
